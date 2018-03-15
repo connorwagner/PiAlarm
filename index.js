537986f6e14bf7cea2a8d47bec10f0affe20c33b
Connor Wagner <connorawagner@icloud.com>
2018-03-15 19:34:30 -0400

    Added MySQL integration

--- index.js
+++ index.js
@@ -17,6 +17,45 @@ function deleteFile(fileName) {
     fs.unlinkSync(__dirname + "/games/" + fileName);
 }
 
+var Gpio = require('pigpio').Gpio;
+
+var ledRed = new Gpio(27, {mode: Gpio.OUTPUT});
+var ledGreen = new Gpio(17, {mode: Gpio.OUTPUT});
+var ledBlue = new Gpio(22, {mode: Gpio.OUTPUT});
+
+var currentRed = 0;
+var currentGreen = 0;
+var currentBlue = 0;
+
+function setColor(red, green, blue) {
+    if (red < 0 || green < 0 || blue < 0 || red > 255 || green > 255 || blue > 255) return false;
+
+    currentRed = red;
+    currentGreen = green;
+    currentBlue = blue;
+
+    /*ledRed.pwmWrite(red);
+    ledGreen.pwmWrite(green);
+    ledBlue.pwmWrite(blue);*/
+
+    return true;
+}
+
+function setRed(red) {
+    return setColor(red, currentGreen, currentBlue);
+}
+
+function setGreen(green) {
+    return setColor(currentRed, green, currentBlue);
+}
+
+function setBlue(blue) {
+    return setColor(currentRed, currentGreen, blue);
+}
+
+var mysql = require('mysql');
+var conn = mysql.createConnection({host: 'localhost', user: 'alarm', password: 'alarmpassword', database: 'Alarm'});
+
 ////////////////////////////////////////////////////////////////
 // Hapi Server
 
@@ -42,29 +81,87 @@ server.route([
     },
     {
         method: 'GET',
-        path: '/alarm',
+        path: '/alarms',
         handler: function(request, reply) {
-            const result = readFile("alarm.txt");
-            if (!result) {
-                reply("No alarm set");
-            } else {
-                reply(result);
-            }
+            conn.query("SELECT * FROM Alarm", function(err, result, fields) {
+                if (err) reply(Boom.badImplementation());
+
+                reply(JSON.stringify(result));
+            });
         }
     },
     {
-        method: 'POST',
-        path: '/alarm',
+        method: 'GET',
+        path: '/alarms/{alarm}',
         config: {
             validate: {
-                payload: {
-                    hour: Joi.number().integer().min(0).max(23).required(),
-                    minute: Joi.number().integer().min(0).max(59).required(),
+                params: {
+                    alarm: Joi.number().integer().min(0).required()
                 }
             }
         },
         handler: function(request, reply) {
-            reply("Success");
+            conn.query("SELECT * FROM Alarm WHERE ID=" + request.params.alarm, function(err, result, fields) {
+                if (err) reply(Boom.badImplementation());
+
+                reply(result[0]);
+            });
+        }
+    },
+    {
+        method: 'POST',
+        path: '/alarms',
+        config: {
+            validate: {
+                payload: {
+                    days: Joi.string().regex(/^[MTWRFSU]{1,7}$/).min(1).max(7).required(),
+                    hour: Joi.number().integer().min(0).max(23).required(),
+                    minute: Joi.number().integer().min(0).max(59).required()
+                }
+            }
+        },
+        handler: function(request, reply) {
+            conn.query("INSERT INTO Alarm (Days, Hour, Minute) VALUES ('" + request.payload.days + "', " + request.payload.hour + ", " + request.payload.minute + ")", function(err, result, fields) {
+                if (err) reply(Boom.badImplementation());
+
+                reply("Success");
+            });
+        }
+    },
+    {
+        method: 'PUT',
+        path: '/alarms/{alarm}',
+        config: {
+            validate: {
+                params: {
+                    alarm: Joi.number().integer().min(0).required()
+                }
+            }
+        },
+        handler: function(request, reply) {
+            conn.query("UPDATE * SET Active=!Active WHERE ID=" + request.params.alarm, function(err, result, fields) {
+                if (err) reply(Boom.badImplementation());
+
+                reply(result[0]);
+            });
+        }
+    },
+    {
+        method: 'DELETE',
+        path: '/alarms/{alarm}',
+        config: {
+            validate: {
+                params: {
+                    alarm: Joi.number().integer().min(0).required()
+                }
+            }
+        },
+        handler: function(request, reply) {
+            conn.query("DELETE FROM Alarm WHERE ID=" + request.params.alarm, function(err, result, fields) {
+                if (err) reply(Boom.badImplementation());
+
+                reply("Success");
+            });
         }
     },
 ]);
