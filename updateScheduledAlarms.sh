c332bbc08a18cccf3374af6ec5348727094e4429
Connor Wagner <connorawagner@icloud.com>
2018-03-17 01:47:17 -0400

    Handled midnight hour edge case for scheduling alarm script

--- updateScheduledAlarms.sh
+++ updateScheduledAlarms.sh
@@ -10,37 +10,39 @@ done <<< "$JOBS"
 
 ALARMS=$(mysql -u alarm -palarmpassword -D Alarm -NB -e "SELECT Days, Hour, Minute FROM Alarm WHERE Active=1")
 while read -r line; do
-    hour=`awk '{print $2}' <<< "$line"`
-    AMPM="AM"
-    if [ $hour -ge 12 ]; then
-        AMPM="PM"
-        if [ $hour -g 12 ]; then
-            hour=$hour-12
+    if [ ! -z "$line" ]; then
+        hour=`awk '{print $2}' <<< "$line"`
+        hour=$((hour-1))
+        swapDay=false
+        AMPM="AM"
+        if [ $hour -le -1 ]; then
+            swapDay=true
+            AMPM="PM"
+            hour=11
         fi
+        if [ $hour -ge 12 ]; then
+            AMPM="PM"
+            if [ $hour -ge 13 ]; then
+                hour=$((hour-12))
+            fi
+        fi
+        minute=`awk '{print $3}' <<< "$line"`
+        if [ $minute -le 9 ]; then
+            minute="0$minute"
+        fi
+        days=`awk '{print $1}' <<< "$line"`
+        for (( i=0; i<${#days}; i++ )); do
+            day="${days:$i:1}";
+            case "$day" in
+                M) $swapDay && day="Sunday" || day="Monday" ;;
+                T) $swapDay && day="Monday" || day="Tuesday" ;;
+                W) $swapDay && day="Tuesday" || day="Wednesday" ;;
+                R) $swapDay && day="Wednesday" || day="Thursday" ;;
+                F) $swapDay && day="Thursday" || day="Friday" ;;
+                S) $swapDay && day="Friday" || day="Saturday" ;;
+                U) $swapDay && day="Saturday" || day="Sunday" ;;
+            esac
+            echo "echo 'python3 /home/pi/PiAlarm/startAlarm.py' | at $hour:$minute $AMPM $day > /dev/null 2>&1"
+        done
     fi
-    minute=`awk '{print $3}' <<< "$line"`
-    if [ $minute -le 9 ]; then
-        minute="0$minute"
-    fi
-    days=`awk '{print $1}' <<< "$line"`
-    for (( i=0; i<${#days}; i++ )); do
-        day="${days:$i:1}";
-        case "$day" in
-            M) day="Monday"
-                ;;
-            T) day="Tuesday"
-                ;;
-            W) day="Wednesday"
-                ;;
-            R) day="Thursday"
-                ;;
-            F) day="Friday"
-                ;;
-            S) day="Saturday"
-                ;;
-            U) day="Sunday"
-                ;;
-        esac
-        echo 'python3 /home/pi/PiAlarm/startAlarm.py' | at $hour:$minute $AMPM $day > /dev/null 2>&1
-    done
 done <<< "$ALARMS"
