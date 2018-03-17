7d8d2e70fb042ae95738d335f703c40fa8f97b02
Connor Wagner <connorawagner@icloud.com>
2018-03-17 18:26:11 -0400

    Restructured php directory

--- updateScheduledAlarms.sh
+++ updateScheduledAlarms.sh
@@ -42,7 +42,7 @@ while read -r line; do
                 S) $swapDay && day="Friday" || day="Saturday" ;;
                 U) $swapDay && day="Saturday" || day="Sunday" ;;
             esac
-            echo 'python3 /home/pi/PiAlarm/startAlarm.py' | at $hour:$minute $AMPM $day > /dev/null 2>&1
+            echo 'python3 /home/pi/PiAlarm/startAlarm.py' | at $hour:$minute $AMPM $day 2> /dev/null
         done
     fi
 done <<< "$ALARMS"
